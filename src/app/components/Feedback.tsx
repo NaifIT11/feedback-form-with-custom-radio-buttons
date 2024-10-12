0677128f93930a2f9a6d35553e37a78785ecfbda
Naif Otibi <nn.n@outlook.cl>
2024-10-12 22:24:57 +0300

    edit bug

--- src/app/components/Feedback.tsx
+++ src/app/components/Feedback.tsx
@@ -1,7 +1,7 @@
+
 "use client"
 
-
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
 import { Annoyed, Frown, Laugh, Meh, Smile } from "lucide-react";
 
@@ -30,12 +30,14 @@ export default function Feedback() {
       SVG: <Annoyed className="w-8 h-8 text-blue-300" />,
     },
   ];
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({
-        feedback: selectedValue
-    })
-  }
+      feedback: selectedValue,
+    });
+  };
+
   return (
     <div className="w-3/4 main-shadow md:w-1/2 rounded-sm p-6 bg-white-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <div className="space-y-4">
@@ -45,22 +47,30 @@ export default function Feedback() {
         </p>
       </div>
       <form className="mt-4 flex flex-col gap-4" onSubmit={handleSubmit}>
-        <RadioGroup 
-          value={selectedValue} 
-          onValueChange={setSelectedValue} 
+        <RadioGroup
+          value={selectedValue}
+          onValueChange={setSelectedValue}
           className="flex gap-2"
         >
           {radioItemsValues.map((ri) => (
-            <RadioGroupItem 
+            <RadioGroupItem
               key={ri.value}
               value={ri.value}
               id={ri.value}
               data-checked={selectedValue === ri.value}
               className="w-full"
             >
-              <div data-checked={selectedValue === ri.value} className="flex data-[checked=true]:border-blue-600 flex-col items-center gap-2 p-2 rounded bg-white border border-blue-300">
+              <div
+                data-checked={selectedValue === ri.value}
+                className="flex data-[checked=true]:border-blue-600 flex-col items-center gap-2 p-2 rounded bg-white border border-blue-300"
+              >
                 {ri.SVG}
-                <div role="heading" aria-level="5" className="text-sm text-blue-300 data-[checked=true]:text-blue-600" data-checked={selectedValue === ri.value}>
+                <div
+                  role="heading"
+                  aria-level="5"
+                  className="text-sm text-blue-300 data-[checked=true]:text-blue-600"
+                  data-checked={selectedValue === ri.value}
+                >
                   {ri.value}
                 </div>
               </div>
@@ -68,7 +78,9 @@ export default function Feedback() {
           ))}
         </RadioGroup>
         <div className="self-end">
-            <button type="submit" className="px-4 py-2 rounded font-medium text-white bg-blue-900">submit</button>
+          <button type="submit" className="px-4 py-2 rounded font-medium text-white bg-blue-900">
+            Submit
+          </button>
         </div>
       </form>
     </div>
